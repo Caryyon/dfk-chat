002ecadfc804a6b2c07f85f76651e1cc45ec96a6
Cary Wolff <cwolff@seer.la>
2022-01-09 22:09:52 -0600

    more setup

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { supabase } from "./supabaseClient";
+import createPersistedState from "use-persisted-state";
+const useUsernameState = createPersistedState("username");
 
 function App({ isExt }) {
   const [msgList, setMsgList] = useState([]);
   const [msg, setMsg] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [username, setUsername] = useUsernameState("");
+
+  // sets up a function for keeping chat at the bottom
   const messagesEndRef = useRef(null);
   const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    if (messagesEndRef) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   };
   useEffect(scrollToBottom, [msgList]);
 
+  // call for messages
   async function getMsgs() {
-    let { data: dfk, error } = await supabase.from("dfk").select("*");
-    setMsgList(dfk);
+  let { data: dfk, error } = await supabase.from("dfk").select("*");
+  setMsgList(dfk);
   }
+   inital setup  for the chat history
   useEffect(() => {
-    getMsgs();
+  getMsgs();
   }, []);
 
+  // sets up a subscription listener
   useEffect(() => {
     const dfk = supabase
       .from("dfk")
@@ -32,36 +43,61 @@ function App({ isExt }) {
     return () => dfk();
   }, []);
 
+  // sends the new message
   const sendMsg = async (e) => {
     e.preventDefault();
     try {
       const { data, error } = await supabase
         .from("dfk")
-        .insert([{ username: "someValue", message: msg }]);
+        .insert([{ username, message: msg }]);
     } catch (err) {
       console.log(err);
     } finally {
       setMsg("");
     }
   };
-  const updateMsg = ({ target: { value } }) => {
-    setMsg(value);
-  };
+  // updates messsage inside input
+  const updateMsg = ({ target: { value } }) => setMsg(value);
+  const updateUsername = ({ target: { value } }) => setUsername(value);
 
   return (
     <>
       <div className="dfkc-App">
         {msgList.map(({ username, message }, i) => (
           <div className="dfkc-ChatItem" key={i}>
-            <span className="dfkc-username">{username}: </span>
-            <p className="dfkc-message"> {message}</p>
+            <span className="dfkc-username">{username}:</span>
+            <p className="dfkc-message">{message}</p>
           </div>
         ))}
       </div>
-      <form className="dfkc-MsgInput" onSubmit={sendMsg} ref={messagesEndRef}>
-        <input value={msg} onChange={updateMsg} />
-        <button type="submit">Send</button>
-      </form>
+      {toggle && username ? (
+        <form className="dfkc-MsgInput" onSubmit={sendMsg} ref={messagesEndRef}>
+          <input
+            placeholder="Message"
+            value={msg}
+            autoFocus
+            onChange={updateMsg}
+          />
+          <button type="submit">Send</button>
+        </form>
+      ) : (
+        <form
+          className="dfkc-MsgInput"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setToggle(true);
+          }}
+          ref={messagesEndRef}
+        >
+          <input
+            placeholder="Pick a Username"
+            value={username}
+            autoFocus
+            onChange={updateUsername}
+          />
+          <button type="submit">Send</button>
+        </form>
+      )}
     </>
   );
 }
